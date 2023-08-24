128f1e4b05783c80ab1d9065b9bc15461b5bed60
xycAZ <kfgp040@astrazeneca.net>
2023-08-24 15:07:46 +0100

    ef

--- 02_relion_gui/setup_relion_v31_gui.sh
+++ 02_relion_gui/setup_relion_v31_gui.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -xue
+sudo apt-get update
+sudo apt install cmake git build-essential mpi-default-bin mpi-default-dev libfftw3-dev libtiff-dev libpng-dev ghostscript libxft-dev
 
 BASE_DIR=/shared
 TARGET=relion-v31
@@ -12,7 +14,7 @@ git checkout ${RELION_TAG}
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake ..
+cmake -DFORCE_OWN_FFTW=ON -DFORCE_OWN_FLTK=ON ..
 make -j 4
 
 echo "export PATH=${PATH}:${BASE_DIR}/${TARGET}/${BUILD_DIR}/bin" |tee -a ~/.bashrc
