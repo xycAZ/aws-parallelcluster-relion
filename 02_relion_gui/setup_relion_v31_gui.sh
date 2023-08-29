3983f10a0f955a61924ce5e241c49953d397949b
xycAZ <kfgp040@astrazeneca.net>
2023-08-29 09:59:36 +0100

    add

--- 02_relion_gui/setup_relion_v31_gui.sh
+++ 02_relion_gui/setup_relion_v31_gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xue
 sudo apt-get update
-sudo apt install cmake git build-essential mpi-default-bin mpi-default-dev libfftw3-dev libtiff-dev libpng-dev ghostscript libxft-dev
+sudo apt-get install cmake git build-essential mpi-default-bin mpi-default-dev libfftw3-dev libtiff-dev libpng-dev ghostscript libxft-dev -y
 sudo apt-get install libx11-dev libxft-dev -y
 
 BASE_DIR=/shared
