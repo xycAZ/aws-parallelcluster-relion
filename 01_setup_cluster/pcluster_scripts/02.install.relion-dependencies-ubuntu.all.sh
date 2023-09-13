70b2ea69318717692fb121349df813f18be851b3
xycAZ <kfgp040@astrazeneca.net>
2023-09-13 15:37:03 +0100

    mount point

--- 01_setup_cluster/pcluster_scripts/02.install.relion-dependencies-ubuntu.all.sh
+++ 01_setup_cluster/pcluster_scripts/02.install.relion-dependencies-ubuntu.all.sh
@@ -8,3 +8,24 @@
 sudo apt-get update
 sudo apt-get install cmake git build-essential mpi-default-bin mpi-default-dev libfftw3-dev libtiff-dev libpng-dev ghostscript libxft-dev -y
 sudo apt-get install libx11-dev libxft-dev -y
+
+# Install S3 Mountpoint if it's not installed
+if [ ! -x "$(which mount-s3)" ]; then
+    sudo apt-get install -y curl libfuse-dev pkg-config fuse libclang-dev
+    #sudo yum install -y fuse fuse-devel cmake3 clang-devel
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source "$HOME/.cargo/env"
+    git clone --recurse-submodules https://github.com/awslabs/mountpoint-s3.git
+    cd mountpoint-s3/
+    cargo build --release
+    mv target/release/mount-s3 /usr/bin/
+fi
+
+# get network throughput from ec2 instance
+instance_type=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region|awk -F\" '{print $4}')
+network=$(aws ec2 --region ${region} describe-instance-types --instance-types ${instance_type} --query "InstanceTypes[].[NetworkInfo.NetworkPerformance]" --output text | grep -o '[0-9]\+')
+
+# Mount S3 Bucket
+mkdir -p ${1}
+chmod 777 ${1}
+mount-s3 --throughput-target-gbps ${network} ${2} ${1}
